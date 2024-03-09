8c66b8c2ca1c246480beb8feb28c014c00f4059e
Pavan Sharma <pavan.bnmk@gmail.com>
2024-03-09 21:59:23 +0530

    about us page completed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { StrictMode } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App(){
   
   return(
   <StrictMode>
     <Navbar title='Text Utils' aboutText='About Us' />
-    <TextForm heading="Enter the text to analyze." />
+    {/* <TextForm heading="Enter the text to analyze." /> */}
+    <About />
   </StrictMode>
 
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,9 +4,21 @@ export default function TextForm( props ) {
 
     const [text, setText ] = useState('Enter text here...2!');
 
+    const handleClearText = ()=>{
+        let newText = "";
+        console.log("Clear clicked...");
+        setText(newText);
+    }
+
     const handleUpClick = ()=>{
         let newText = text.toUpperCase();
         console.log("Upper case clicked...");
+        setText(newText);
+    }
+
+    const handleLwClick = ()=>{
+        let newText = text.toLowerCase();
+        console.log("Lower case clicked...");
         setText(newText);
     }
 
@@ -16,12 +28,24 @@ export default function TextForm( props ) {
     }
 
   return (
+    <>
     <div className='container'>
         <h1>{props.heading}</h1>
         <div className="mb-3">
             <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="8"></textarea>
         </div>
-        <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className='btn btn-primary mx-2' onClick={handleLwClick}>Convert to Uppercase</button>
+        <button className='btn btn-primary mx-2' onClick={handleClearText}>Clear</button>
     </div>
+
+    <div className='container my-3'>
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").length} words, {text.length} characters</p>
+        <p>{ 0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
   )
 }
